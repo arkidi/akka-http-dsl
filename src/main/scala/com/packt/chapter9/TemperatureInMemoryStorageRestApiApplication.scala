3eb6d81b858a5a53a07b9c1db9a3e51006f8c078
Seungyoung Yang <arkidi@gmail.com>
2019-06-04 15:42:44 +0900

    Akka HTTP Routing DSL Example

---
+++ src/main/scala/com/packt/chapter9/TemperatureInMemoryStorageRestApiApplication.scala
@@ -0,0 +1,23 @@
+package com.packt.chapter9
+
+import akka.http.scaladsl.server.{HttpApp}
+import akka.http.scaladsl.settings.ServerSettings
+import com.typesafe.config.ConfigFactory
+import scala.collection.mutable
+
+class TemperatureInMemoryStorageRestApi(cache: mutable.Map[String, TemperatureMeasurement]) extends HttpApp
+  with InMemoryStorageRestApi[String, TemperatureMeasurement]
+  with GetRequestsHandler
+  with PostRequestHandler
+  with PutRequestsHandler
+  with DeleteRequestsHandler {
+
+  implicit val fixedPath = "temperature"
+  val route = composeRoute(cache)
+}
+
+object TemperatureInMemoryStorageRestApiApplication extends App {
+  val cache = mutable.Map.empty[String, TemperatureMeasurement]
+
+  new TemperatureInMemoryStorageRestApi(cache).startServer("0.0.0.0", 8089, ServerSettings(ConfigFactory.load()))
+}
